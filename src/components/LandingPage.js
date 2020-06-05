33cad3a44db08244b304be415e606b435b7cce1f
tsaihuangsd <tsaihuangsd@gmail.com>
2020-06-04 17:39:53 -0700

    inherit code from previous project

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,9 @@
 <div>
-                                        <h1 id="title">Welcome to MyPostedNotes</h1>
-                                        <nav>
-                                                <div id="login" onClick={function(){
-                                                        lock.show();}}>LOG IN
-                                                </div>
-                                        </nav>
-                                        
-                                </div>
+        <h1 id="title">Welcome to MyPostedNotes</h1>
+        <nav>
+                <div id="login" onClick={function(){
+                        lock.show();}}>LOG IN
+                </div>
+        </nav>
+        
+</div>
